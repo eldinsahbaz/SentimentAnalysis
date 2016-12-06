93383c4637de6d31ec2a9154356a47b1455f4011
eli goldweber <eligoldweber@gmail.com>
2016-12-06 11:50:09 -0500

    limmit seraches to english

--- PHP_Twitter/littleApp.php
+++ PHP_Twitter/littleApp.php
@@ -28,7 +28,7 @@ $string = json_decode($twitter->setGetfield($getfield)
 ->buildOauth($url, $requestMethod)
 ->performRequest(),$assoc = TRUE);
 //if($string["errors"][0]["message"] != "") {echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";exit();}
-$file = 'test.txt';
+$file = 'output.txt';
 file_put_contents($file, "");
 
 foreach($string as $items)
